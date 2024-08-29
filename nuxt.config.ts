16ae92a125128b5b23e9028c42162a4acca62660
Robin Simonklein <rsimonklein@gmail.com>
2024-08-29 23:59:08 +0200

    feat: Add NuxtHub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  // future: {
-  //   compatibilityVersion: 4,
-  // },
+
   devtools: { enabled: true },
 
   runtimeConfig: {
-    musicLinksApi: '',
+    musicLinksApi: process.env.NUXT_MUSIC_LINKS_API,
+  },
+
+  modules: ['@nuxthub/core'],
+
+  hub: {
+    cache: true,
   },
 });
